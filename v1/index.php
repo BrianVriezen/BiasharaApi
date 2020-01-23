bb5e403465fd0efa08862d3f118c5dde88a8776e
BrianVriezen <brianvriezen@hotmail.com>
2020-01-23 11:59:29 +0100

    Works now with json (requires the json version of the app)

--- v1/index.php
+++ v1/index.php
@@ -5,7 +5,8 @@
 	
 	//response array 
 	$response = array(); 
-	
+	$postData = json_decode(file_get_contents("php://input"), true);	
+
 	//if a get parameter named op is set we will consider it as an api call 
 	if(isset($_GET['op'])){
 		
@@ -17,25 +18,28 @@
 			case 'adduser':
 
 				
-				if(isset($_POST['phone']) && isset($_POST['password']) && isset($_POST['firstname']) && isset($_POST['lastname']) && isset($_POST['gender']) && isset($_POST['date']))
+				if(isset($postData['phone']) && isset($postData['password']) && isset($postData['firstname']) && isset($postData['lastname']) && isset($postData['gender']) && isset($postData['date']))
 				{
 					$options = [
 						'cost' => 12 // the default cost is 10
 					];
-					$hash = password_hash($_POST['password'], PASSWORD_DEFAULT, $options);
-					$phoneInt = (int)$_POST['phone'];
+					$hash = password_hash($postData['password'], PASSWORD_DEFAULT, $options);
+					$phoneInt = (int)$postData['phone'];
 					$db = new Operation(); 
           $id = 003;
           //com_create_guid();
-					if($db->Register($id, $phoneInt, $hash, $_POST['email'], $_POST['firstname'], $_POST['middlename'], $_POST['lastname'],$_POST['address'], $_POST['city'], $_POST['state'], $_POST['zipcode'], $_POST['country'], $_POST['gender'], $_POST['date']))
+					if($db->Register($id, $phoneInt, $hash, $postData['email'], $postData['firstname'], $postData['middlename'], $postData['lastname'], $postData['address'], $postData['city'], $postData['state'], $postData['zipcode'], $postData['country'], $postData['gender'], $postData['date']))
 					{
+						$response['success'] = true;
 						$response['error'] = false;
 						$response['message'] = 'User added successfully';
 					}else{
+						$response['success'] = false;
 						$response['error'] = true;
 						$response['message'] = 'Could not add user';
 					}
 				}else{
+					$response['success'] = false;
 					$response['error'] = true; 
 					$response['message'] = 'Required Parameters are missing';
 				}
@@ -45,20 +49,23 @@
       //if it is getlogin that means we are fetching the records
       
       case 'getlogin':
-		if(isset($_POST['phone']) && isset($_POST['password']))
+		if(isset($postData['phone']) && isset($postData['password']))
 		{
-			$phoneInt = (int)$_POST['phone'];
+			$phoneInt = (int)$postData['phone'];
 			$db = new Operation(); 
 			$id = 3;//com_create_guid();
-			if($db->getLogin($phoneInt, $_POST['password']))
+			if($db->getLogin($phoneInt, $postData['password']))
 			{
+				$response['success'] = true;
 				$response['error'] = false;
 				$response['message'] = 'User login successfully';
 			}else{
+				$response['success'] = false;
 				$response['error'] = true;
 				$response['message'] = 'Could not login user';
 			}
 		}else{
+			$response['success'] = false;
 			$response['error'] = true; 
 			$response['message'] = 'Required Parameters are missing';
 		}
@@ -66,6 +73,7 @@
 		}
 		
 	}else{
+		$response['success'] = false;
 		$response['error'] = false; 
 		$response['message'] = 'Invalid Request';
 	}
